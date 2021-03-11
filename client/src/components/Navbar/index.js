d2d5749a7c37262e3bfa6fcf4ce6d1d0db15bafd
Corey Smith <CSSmith1997@gmail.com>
2021-03-10 17:44:44 -0700

    F

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -6,13 +6,13 @@ import './nav.css';
 function Navigation({ location }) {
     return (
         <Navbar bg='dark' expand='md' activeKey={location.pathname}>
-            <Navbar.Brand className ="navlinks"  href='/about'>Corey Smith</Navbar.Brand>
+            <Navbar.Brand className ="navlinks"  href='/About'>Corey Smith</Navbar.Brand>
             <Navbar.Toggle aria-controls='basic-navbar-nav' />
             <Navbar.Collapse className='justify-content-end' id='basic-navbar-nav'>
                 <Nav>
-                    <Nav.Link className ="navlinks" href='/about'>About</Nav.Link>
-                    <Nav.Link className ="navlinks"  href='/portfolio'>Portfolio</Nav.Link>
-                    <Nav.Link className ="navlinks" href='/contact'>Contact</Nav.Link>
+                    <Nav.Link className ="navlinks" href='/About'>About</Nav.Link>
+                    <Nav.Link className ="navlinks"  href='/Portfolio'>Portfolio</Nav.Link>
+                    <Nav.Link className ="navlinks" href='/Contact'>Contact</Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
